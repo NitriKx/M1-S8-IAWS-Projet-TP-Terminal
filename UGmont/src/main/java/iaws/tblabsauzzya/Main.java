79c438a9653f4c203ef4d7b508901d6644bbd1e5
Benoît Sauvère <benoit.sauvere@teevity.com>
2015-03-18 15:00:59 +0100

    Traduction des commentaires par défaut en français. Activation du support de JSON dasn Grizzly.

--- UGmont/src/main/java/iaws/tblabsauzzya/Main.java
+++ UGmont/src/main/java/iaws/tblabsauzzya/Main.java
@@ -1,5 +1,6 @@
 package iaws.tblabsauzzya;
 
+import com.sun.jersey.api.json.JSONConfiguration;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -12,22 +13,22 @@ import java.net.URI;
  *
  */
 public class Main {
-    // Base URI the Grizzly HTTP server will listen on
+    // URI de base sur laquelle Grizzly va écouter
     public static final String BASE_URI = "http://localhost:8080/myapp/";
 
     /**
-     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * Démarre le serveur Grizzly HTTP server qui expose les resource JAX-RS définie dans l'application
      * @return Grizzly HTTP server.
      */
     public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in iaws.tblabsauzzya package
-        final ResourceConfig rc = new ResourceConfig().packages("iaws.tblabsauzzya");
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
+        final ResourceConfig rc = new ResourceConfig()
+                // Indique à Grizzly le package qu'il doit scanner pour trouver les classes
+                .packages("iaws.tblabsauzzya");
+
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
+
 
     /**
      * Main method.
@@ -36,8 +37,7 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.out.println(String.format("Jersey app started with WADL available at %sapplication.wadl\nHit enter to stop it...", BASE_URI));
         System.in.read();
         server.stop();
     }
