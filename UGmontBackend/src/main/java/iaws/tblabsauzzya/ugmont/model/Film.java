aec0a9ad25c2bf100511d921a3a52aa1f8f7611a
Terry Blain <terry.blain31@gmail.com>
2015-04-15 16:35:27 +0200

    création classe Film

---
+++ UGmontBackend/src/main/java/iaws/tblabsauzzya/ugmont/model/Film.java
@@ -0,0 +1,47 @@
+package iaws.tblabsauzzya.ugmont.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by terry on 15/04/15.
+ */
+@XmlRootElement
+public class Film {
+
+    private String title;
+    private String type;
+    private String year;
+    private String imdbID;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+}
